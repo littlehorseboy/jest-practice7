7be23500d207f09208bd15870df2cea140705196
horse <littlehorseboy@gmail.com>
2019-03-22 17:53:59 +0800

    feat: Testing React Apps

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Content from '../../components/Content/Content.jsx';
+
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default About;
